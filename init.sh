03fc3b648c2e81aff7c75eec7587b307f206ef3b
Toru Komatsu <k0ma@utam0k.jp>
2023-04-21 01:30:26 +0000

    update

--- init.sh
+++ init.sh
@@ -1,20 +1,37 @@
 #/bin/bash
 
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | sudo apt-key add -
-sudo apt update
-sudo apt -y upgrade
-sudo apt install -y podman bpftrace dkms linux-headers-$(uname -r)
-sudo mount -t debugfs none /sys/kernel/debug
-sudo modprobe kheaders
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | apt-key add -
+apt update
+apt -y upgrade
+apt install -y podman bpftrace
+mount -t debugfs none /sys/kernel/debug
 
-git clone https://github.com/brauner/mount-idmapped.git
-cd mount-idmapped
-gcc mount-idmapped.c -o idmapped
-sudo mv idmapped /usr/bin/
-cd ../
-
-git clone https://github.com/toby63/shiftfs-dkms.git
-cd shiftfs-dkms
-git checkout k5.4
-./update1
+apt-get install -y \
+  bison \
+  cmake \
+  flex \
+  g++ \
+  git \
+  libelf-dev \
+  zlib1g-dev \
+  libfl-dev \
+  systemtap-sdt-dev \
+  binutils-dev \
+  libcereal-dev \
+  llvm-dev \
+  llvm-runtime \
+  libclang-dev \
+  clang \
+  libpcap-dev \
+  libgtest-dev \
+  libgmock-dev \
+  asciidoctor \
+  xxd \
+  pahole
+git clone https://github.com/iovisor/bpftrace --recurse-submodules /tmp/bpftrace/
+mkdir /tmp/bpftrace/build; cd /tmp/bpftrace/build;
+cd /tmp/bpftrace/build; ../build-libs.sh;
+cd /tmp/bpftrace/build; cmake -DCMAKE_BUILD_TYPE=Release ..;
+cd /tmp/bpftrace/build; make -j8;
+cd /tmp/bpftrace/build; make install;
