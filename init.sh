ce071c8a278feab4c37a4bedffc9868af9359459
utam0k <k0ma@utam0k.jp>
2023-10-01 23:52:47 +0000

    Clean
    Signed-off-by: utam0k <k0ma@utam0k.jp>

--- init.sh
+++ init.sh
@@ -1,20 +1,2 @@
 #/bin/bash
 
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | sudo apt-key add -
-sudo apt update
-sudo apt -y upgrade
-sudo apt install -y podman bpftrace dkms linux-headers-$(uname -r)
-sudo mount -t debugfs none /sys/kernel/debug
-sudo modprobe kheaders
-
-git clone https://github.com/brauner/mount-idmapped.git
-cd mount-idmapped
-gcc mount-idmapped.c -o idmapped
-sudo mv idmapped /usr/bin/
-cd ../
-
-git clone https://github.com/toby63/shiftfs-dkms.git
-cd shiftfs-dkms
-git checkout k5.4
-./update1
